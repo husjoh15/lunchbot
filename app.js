4bbaa27cd89213019f387fb8eeb1e3f01fa43b25
Johannes Husa <johannes.husa@payex.com>
2019-08-30 15:01:17 +0200

    add weekly menu and remove stuff that is not needed anymore

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 var SlackBot = require('slackbots');
-var Parse = require('parse/node');
 var express = require('express');
-var schedule = require('node-schedule');
-//var mongoose = require('mongoose');
-
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var app = express();
 
 // mongoose.connect(process.env.MONGO_DB_PATH, {useNewUrlParser: true});
@@ -28,7 +25,8 @@ var app = express();
 //     console.log(pizza.posted);
 //   });
 
-
+var weeklyMenu = "";
+var url = 'https://k6.retailsolution.no/meny';
 
 app.get('/', function (req, res) {
     res.send('Hello World');
@@ -36,24 +34,84 @@ app.get('/', function (req, res) {
 
 app.listen(process.env.PORT, '0.0.0.0');
 
-Parse.serverURL = 'https://parseapi.back4app.com';
-
-Parse.initialize(
-    'vN7TX86eluj0mrrW6cnBdl87KaXJe6gihnGHRfeV', // This is your Application ID
-    'SgBWNm5AHX46MvvZInlBcEzvUaEVn8UwNkikJzef', // This is your Javascript key
-    'CpLQoAythtn64XSHl6PQkXEZNIHEvype1pZ7Ey3L' // This is your Master key (never use it in the frontend)
-);
-
 const bot = new SlackBot({
     token: process.env.SLACK_OAUTH_ACCESS_TOKEN,
     name: 'lunchbot'
 });
 
+function getWeeklyMenu()
+{
+    var req = new XMLHttpRequest();
+
+    req.open('GET', url, false);
+    req.send(null);
+
+    if(req.status == 200) {
+        return formatResponseText(req.responseText);
+    }
+    return "";
+}
+
+function getDailyMenu(){
+    getWeeklyMenu();
+    var time = new Date();
+    if (time.getDay() === 1)
+        return getMondayMenu();
+    if (time.getDay() === 2)
+        return getTuesdayMenu();
+    if (time.getDay() === 3)
+        return getWednesdayMenu();
+    if (time.getDay() === 4)
+        return getThursdayMenu();
+    if (time.getDay() === 5)
+        return getFridayMenu();
+}
+
+function getMondayMenu(){
+    var menu = weeklyMenu.split(/Mandag:*/g)[1];
+    var menu1 = menu.split(/Tirsdag:*/g)[0].replace('*','').replace('*','');
+    return menu1;
+}
+function getTuesdayMenu(){
+    var menu = weeklyMenu.split(/Tirsdag:*/g)[1];
+    var menu1 = menu.split(/Onsdag:*/g)[0].replace('*','').replace('*','');
+    return menu1;
+}
+function getWednesdayMenu(){
+    var menu = weeklyMenu.split(/Onsdag:*/g)[1];
+    var menu1 = menu.split(/Torsdag:*/g)[0].replace('*','').replace('*','');
+    return menu1;
+}
+function getThursdayMenu(){
+    var menu = weeklyMenu.split(/Torsdag:*/g)[1];
+    var menu1 = menu.split(/Fredag:*/g)[0].replace('*','').replace('*','');
+    return menu1;
+}
+function getFridayMenu(){
+    var menu = weeklyMenu.split(/Fredag:*/g)[1].replace('*','');
+    return menu;
+}
+
+function formatResponseText(responseText){
+    var changed = responseText.replace(/<[^>]*>?/gm, '');
+    var weeklyMenuGibberish = changed.match("Uke (.*)Dagens");
+    var weeklyRemoveGibberish = weeklyMenuGibberish[0].replace(/&amp;/g,'&').replace(/&nbsp;/g,' ');
+    weeklyMenu = weeklyRemoveGibberish.replace(/Mandag:/g, '\n *Mandag:* \n')
+    .replace(/Tirsdag:/g, '\n\n *Tirsdag*: \n')
+    .replace(/Onsdag:/g, '\n\n *Onsdag:* \n')
+    .replace(/Torsdag:/g, '\n\n *Torsdag:* \n')
+    .replace(/Fredag:/g, '\n\n *Fredag:* \n')
+    .replace(/Hoved:/g, '_Hoved:_ \n')
+    .replace(/Vegetarisk:/g, '\n _Vegetarisk:_ \n')
+    .replace(/Suppe:/g, '\n _Suppe:_ \n');
+    return weeklyMenu;
+}
+
 bot.on('message', (data) => {
     if(data.type !== 'message'){
         return;
     }
-    if(data.username !== 'lunchbot' && data.subtype != 'message_replied')
+    if(data.username !== 'lunchbot' && data.subtype != 'message_replied' && data.subtype !== 'message_changed')
     {
         console.log(data);
         if(data.text.includes("<@UK5C25SSU>"))
@@ -66,29 +124,15 @@ bot.on('message', (data) => {
 })
 
 function handleMessage(data) {
-    if(data.text.includes(' dagens lunsj')) {
-        var danielEmoji = {
-            icon_emoji: ':party_parrot:'
-        }
-        var akselEmoji = {
-            icon_emoji: ':hypers:'
-        }
-
-        if (data.user == 'UC1P036RX')
-        {
-            bot.postMessage(data.channel, "Dagens lunsj er: ```2 Polarbrød med smøreost```", danielEmoji);
-        }
-        else if (data.user == 'UCLP4J332')
-        {
-            bot.postMessage(data.channel, "Dagens lunsj er: ```Rislunsj```", akselEmoji);
-        }
-        else if (data.user == 'U276V7G2F'){
-            todaysLunch(data.channel, true);
-        }
-        else {
-            todaysLunch(data.channel, false);
-        }
-
+    if(data.text.toLowerCase().includes(' ukens')){
+        bot.postMessage(data.channel, getWeeklyMenu(), emoji);
+        
+    }
+    else if(data.text.toLowerCase().includes(' dagens')){
+        bot.postMessage(data.channel, getDailyMenu(), emoji);
+    }
+    else if (data.text.toLowerCase().includes('cola')){
+        bot.postMessage(data.channel, cola(), emoji);
     }
     else {
         var emoji = {
@@ -96,114 +140,6 @@ function handleMessage(data) {
         }
         bot.postMessage(data.channel, 'Jeg er under utvikling og svarer bare på kommandoen `@Lunchbot dagens lunsj`', emoji);
     }
-    // if(message.includes(' add lunch')){
-    //     var time = new Date();
-    //     var lunch = 'kålstuing';
-    //     addLunch(lunch, getFullDate(time));
-    // }
-}
-
-function todaysLunch (channel, geir) {
-    var paramsNoLunch = {
-        icon_emoji: ':pepehands:'
-    }
-    var paramsManyLunch = {
-        icon_emoji: ':monkas:'
-    }
-
-    var date = new Date();
-    const Lunchlist = Parse.Object.extend('Lunchlist');
-    const query = new Parse.Query(Lunchlist);
-    query.equalTo('dato', getFullDate(date));
-    query.find().then((results) => {
-      if (typeof document !== 'undefined') document.write(`Lunchlist found1: ${JSON.stringify(results)}`);
-      stringified = JSON.stringify(results);
-      parsed = JSON.parse(stringified);
-      if (parsed.length === 1)
-      {
-        lunch = parsed[0].lunch;
-        posted = true;
-        if (geir)
-          bot.postMessage(channel, 'Dagens lunsj er: ```' + lunch + ' ``` ' + 'Håper ikke du får forstoppelse!', getParams(lunch.toLowerCase()));
-        else 
-          bot.postMessage(channel, 'Dagens lunsj er: ```' + lunch + ' ``` ', getParams(lunch.toLowerCase()));
-      }
-      else if (parsed.length === 0)
-      {
-        bot.postMessage(channel, 'Det er ikke registrert noen lunsj i dag. Kanskje det er noe registrert her? https://k6.retailsolution.no/meny ', paramsNoLunch);
-      }
-      else
-      {
-        bot.postMessage(channel, 'Det er registrert flere enn 1 lunsj i dag :thinking_face: ', paramsManyLunch);
-      }
-
-    }, (error) => {
-      if (typeof document !== 'undefined') document.write(`Error while fetching Lunchlist: ${JSON.stringify(error)}`);
-      console.error('Error while fetching Lunchlist', error);
-    });
-    //var lunches = Lunch.find({name: 'pizza'}).findOne();
-    //console.log(lunches.schema.object)
-}
-
-function getParams (lunch) {
-    if (lunch.includes('burger'))
-        return {icon_emoji: ':hamburger:'}
-    else if (lunch.includes('kalkun'))
-        return {icon_emoji: ':turkey:'}
-    else if (lunch.includes('storfe'))
-        return {icon_emoji: ':cut_of_meat:'}
-    else if (lunch.includes('potet'))
-        return {icon_emoji: ':potato:'}
-    else if (lunch.includes('bacon'))
-        return {icon_emoji: ':bacon:'}
-    else if (lunch.includes('meat'))
-        return {icon_emoji: ':cut_of_meat:'}
-    else if (lunch.includes('pølse'))
-        return {icon_emoji: ':hotdog:'}
-    else if (lunch.includes('fries'))
-        return {icon_emoji: ':fries:'}
-    else if (lunch.includes('kylling'))
-        return {icon_emoji: ':chicken:'}
-    else if (lunch.includes('chicken'))
-        return {icon_emoji: ':chicken:'}
-    else if (lunch.includes('pork'))
-        return {icon_emoji: ':cut_of_meat:'}
-    else if (lunch.includes(' is '))
-        return {icon_emoji: ':icecream:'}
-    else if (lunch.includes('pommes frites'))
-        return {icon_emoji: ':fries:'}
-    else if (lunch.includes('pizza'))
-        return {icon_emoji: ':pizza:'}
-    else if (lunch.includes('innovation lunch'))
-        return {icon_emoji: ':bulb:'}
-    else if (lunch.includes('gulrot'))
-        return {icon_emoji: ':carrot:'}
-    else
-        return {icon_emoji: ':robot_face:'}
-}
-
-
-// function addLunch (lunch, date) {
-//     var Lunchlist = Parse.Object.extend('Lunchlist');
-//     var lunchlist = new Lunchlist();
-
-//     lunchlist.set('lunch', lunch);
-//     lunchlist.set('dato', date);
-
-//     lunchlist.save().then(
-//     (result) => {
-//         if (typeof document !== 'undefined') document.write(`ParseObject created: ${JSON.stringify(result)}`);
-//         console.log('ParseObject created', result);
-//     },
-//     (error) => {
-//         if (typeof document !== 'undefined') document.write(`Error while creating ParseObject: ${JSON.stringify(error)}`);
-//         console.error('Error while creating ParseObject: ', error);
-//     }
-//     );
-// }
-
-function getFullDate(date) {
-    return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
 }
 
 function cola() {
